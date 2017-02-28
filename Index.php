d0fbaf5cc7c887b6b6053b71cd75cbda50de2a86
1511154 <1511154@rgu.ac.uk>
2017-02-28 16:01:21 +0000

    else if

--- Index.php
+++ Index.php
@@ -6,6 +6,23 @@
     <?php
         echo "Hello World";
     ?>
+
+    <?php
+
+        $myage = 24;
+
+        if($myage > 16 && $myage < 18){
+        print "You can buy specs";
+        }
+    else if($myage > 18 && $myage <21){
+        print "You can buy mugs";
+    }
+    else {
+        print "You can buy sausage rolls";
+    }
+
+
+    ?>
 </p>
 </body>
 </head>
